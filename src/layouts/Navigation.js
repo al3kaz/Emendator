39ed6c0f7d09a40bb8800fbd41e4b3d55e8e7963
Aleksander Kazmierczak <aleksander.kazmierczak@gmail.com>
2020-07-23 21:44:05 +0200

    Navigation proper work

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -4,9 +4,9 @@ import '../styles/Navigation.css'
 
 const navList = [
    { name: "start", exact: true, path: "/" },
-   { name: "oferta", path: "offer" },
-   { name: "do pobrania", path: "download" },
-   { name: "kontakt", path: "contact" },
+   { name: "oferta", path: "/offer" },
+   { name: "do pobrania", path: "/download" },
+   { name: "kontakt", path: "/contact" },
 ]
 
 const Navigation = () => {
