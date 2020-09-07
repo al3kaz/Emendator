b309d4f3cb9d13f3ae6d751b41fe956d30c5edff
Aleksander Kazmierczak <aleksander.kazmierczak@gmail.com>
2020-09-07 14:25:10 +0200

    Updates

---
+++ precache-manifest.f3ae5f2c7127ffe6f5e1740720301e16.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01458aa1603ef21da951927b392916a0",
+    "url": "/Emendator/index.html"
+  },
+  {
+    "revision": "3b4015d5e028e13883bb",
+    "url": "/Emendator/static/css/main.9747d7ec.chunk.css"
+  },
+  {
+    "revision": "a7b5037ec27e5377759f",
+    "url": "/Emendator/static/js/2.8c6bdcda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Emendator/static/js/2.8c6bdcda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4015d5e028e13883bb",
+    "url": "/Emendator/static/js/main.ba9ff777.chunk.js"
+  },
+  {
+    "revision": "cdaf9c725afb2eff4033",
+    "url": "/Emendator/static/js/runtime-main.ae908136.js"
+  },
+  {
+    "revision": "7247606ad009517c859310fc6788dffe",
+    "url": "/Emendator/static/media/hammer_1280.7247606a.jpg"
+  },
+  {
+    "revision": "100a0f182bad0da5c01a68c9375aa636",
+    "url": "/Emendator/static/media/hammer_640.100a0f18.jpg"
+  },
+  {
+    "revision": "4fa0b38f6cdeb197215eeaefda8b35ca",
+    "url": "/Emendator/static/media/justice2_1280.4fa0b38f.jpg"
+  },
+  {
+    "revision": "f20a25ab7d6fbc916fa798f42a1b05cd",
+    "url": "/Emendator/static/media/justice2_640.f20a25ab.jpg"
+  },
+  {
+    "revision": "ea56d422458486e9f7166604d0055df6",
+    "url": "/Emendator/static/media/justice_1280.ea56d422.jpg"
+  },
+  {
+    "revision": "bd3156c7ea6c167d9a321bf5ec31c1df",
+    "url": "/Emendator/static/media/justice_640.bd3156c7.jpg"
+  },
+  {
+    "revision": "dcfa2dffaa7d2863d986cbc75e36681a",
+    "url": "/Emendator/static/media/lawyers_1280.dcfa2dff.jpg"
+  },
+  {
+    "revision": "f5c6cc8eda351518de1401d4955e4a82",
+    "url": "/Emendator/static/media/lawyers_640.f5c6cc8e.jpg"
+  },
+  {
+    "revision": "8846816667c439e9c87a6f6a498f0280",
+    "url": "/Emendator/static/media/logo_Emendator.88468166.png"
+  }
+]);
